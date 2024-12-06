4ade59a8f29bf478a62c8b4cb0f35eb09bb518ce
robertsims <ssekabirarobert037@gmail.com>
2024-12-06 12:05:32 +0300

    added json databases for the api resources

--- model/connection/api.model.pool.connection.js
+++ model/connection/api.model.pool.connection.js
@@ -1,30 +1,29 @@
-"use strict";
-const mysql2 = require("mysql2");
-// configure dotenv for secrete keys accession
-require("dotenv").config();
-require("dotenv").configDotenv();
+// "use strict";
+// const mysql2 = require("mysql2");
+// // configure dotenv for secrete keys accession
+// require("dotenv").config();
+// require("dotenv").configDotenv();
 
-// make pool connection to db
-const pool_connection = mysql2.createPool({
-    database: process.env.DB,
-    password: process.env.DB_CONNECTION_PASSWORD,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER
-});
+// const pool_connection = mysql2.createPool({
+//     database:" process.env.DB",
+//     password: "process.env.DB_CONNECTION_PASSWORD",
+//     host: "localhost",
+//     user: "root"
+// });
 
-try {
-    // get any connection to a specified database
-    pool_connection.getConnection((error, connection) => {
-        if(error) {
-            return console.log(error.message);
-        } else {
-            return console.log("Connected to db successfully!");
-        }
-    });
-} catch (error) {
-    return console.log(`ERROR WHILE CONNECTING TO DB: ${error.message}`);
-}
+// try {
+//     // get any connection to a specified database
+//     pool_connection.getConnection((error, connection) => {
+//         if(error) {
+//             return console.log(error.message);
+//         } else {
+//             return console.log("Connected to db successfully!");
+//         }
+//     });
+// } catch (error) {
+//     return console.log(`ERROR WHILE CONNECTING TO DB: ${error.message}`);
+// }
 
-/* export the promise pool connection function to provide asynchronous
-pool connections to the db */
-module.exports = pool_connection.promise();
+// /* export the promise pool connection function to provide asynchronous
+// pool connections to the db */
+// module.exports = pool_connection.promise();
